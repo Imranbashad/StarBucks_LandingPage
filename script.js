c909f432e64314145b7da71fd81b8d587654f176
imranbashad <imranbasha.d7@gmail.com>
2024-05-16 19:09:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(imag){
+    document.querySelector(".starbucks").src = imag
+}
+function changeColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
